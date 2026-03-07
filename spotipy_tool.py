b6b2a278fca530318dbd3ed669fc1e596077a03f
Ido <elaratido@gmail.com>
2026-03-07 23:40:39 +0200

    added all tracks to list

--- spotipy_tool.py
+++ spotipy_tool.py
@@ -10,6 +10,23 @@ scope = "user-library-read"
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
 
 results = sp.current_user_saved_tracks(limit=50)
+total_tracks = results['total']
 all_tracks = []
 
-print(f"\nTotal Tracks {results['total']}")
+while results:
+    for item in results['items']:
+        track = item['track']
+        all_tracks.append({
+            'id': track['id'],
+            'name': track['name'],
+            'artist': track['artists'][0]['name']
+        })
+
+    print(f"Fetched {len(all_tracks)} / {total_tracks} tracks...", end="\r")
+    
+    if results['next']:
+        results = sp.next(results)
+    else:
+        results = None
+
+print(f"Successfully fetched {len(all_tracks)} tracks.")
